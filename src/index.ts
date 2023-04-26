66f277ebabc37f215b8a9dc89c0e9cc2d4c5f7e1
augustoprogramador <augusto.camargo@aluno.ifsp.edu.br>
2023-04-26 16:51:01 -0300

    demonstração dos tipos de variáveis

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,36 @@
-type Heroi = {
-    nome: string,
-    vulgo: string
-};
 
-function printaObjeto(pessoa: Heroi) {
-    console.log(pessoa);
-    
+/**
+ * VARIÁVEIS
+ */
+
+// TIPOS PRIMITIVOS boolean, number, string
+let ligado: boolean = false;
+let nome: string = 'Augusto';
+let idade: number = 32;
+let altura: number = 1.9;
+
+// null
+// undefined
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+// TIPOS ABRANGENTES any, void
+let retorno: void
+let retornoView: any = false;
+
+// objeto
+let produto: object = {
+    name: 'Augusto'
 }
 
-printaObjeto({
-    nome: 'Saitama',
-    vulgo: 'One Punchman'
-});
+type ProdutoLoja = {
+    nome: string,
+    preco: number,
+    unidades: number
+}
+
+let meuProduto: ProdutoLoja = {
+    nome: "Tênis",
+    preco: 89,
+    unidades: 2
+}
